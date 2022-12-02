e81f49421476b1abff4fa4fdef8662a5ea9b3e0d
NotXia <35894453+NotXia@users.noreply.github.com>
2022-12-02 20:35:41 +0100

    Added packet duplication

--- libvdeplug_wirefilter.c
+++ libvdeplug_wirefilter.c
@@ -39,6 +39,7 @@ struct vde_wirefilter_conn {
 	void *handle;
 	struct vdeplug_module *module;
 	VDECONN *conn;
+	double duplication_probability;
 };
 
 // Module structure
@@ -51,6 +52,8 @@ struct vdeplug_module vdeplug_ops = {
 	.vde_close = vde_wirefilter_close
 };
 
+int computeDuplicationCount(const double duplication_probability);
+
 
 static VDECONN *vde_wirefilter_open(char *vde_url, char *descr, int interface_version, struct vde_open_args *open_args) {
 	(void) descr;
@@ -58,24 +61,28 @@ static VDECONN *vde_wirefilter_open(char *vde_url, char *descr, int interface_ve
 	(void) open_args;
 
 	struct vde_wirefilter_conn *newconn = NULL;
-	char *nested_vnl;
-	struct vdeparms parms[] = {
-		{NULL, NULL}
-	};
 	VDECONN *conn;
+	char *nested_vnl;
+	char *duplication_probability_str = "0.0";
+	struct vdeparms parms[] = {
+		{ "dup", &duplication_probability_str },
+		{ NULL, NULL }
+	};
 
 	nested_vnl = vde_parsenestparms(vde_url);						// Gets the nested VNL
 	if (vde_parsepathparms(vde_url, parms) != 0) { return NULL; } 	// Retrieves the plugin parameters
 	
-	// Open a connection with the nested VNL
+	// Opens a connection with the nested VNL
 	conn = vde_open(nested_vnl, descr, open_args);
 	if (conn == NULL) { return  NULL; }
 	if ((newconn=calloc(1, sizeof(struct vde_wirefilter_conn))) == NULL) {
 		errno = ENOMEM;
 		goto error;
 	}
-	newconn->conn=conn;
 
+	newconn->conn=conn;
+	newconn->duplication_probability = atof(duplication_probability_str);
+	
 	return (VDECONN *)newconn;
 
 error:
@@ -90,7 +97,15 @@ static ssize_t vde_wirefilter_recv(VDECONN *conn, void *buf, size_t len, int fla
 
 static ssize_t vde_wirefilter_send(VDECONN *conn, const void *buf, size_t len, int flags) {
 	struct vde_wirefilter_conn *vde_conn = (struct vde_wirefilter_conn *)conn;
-	return vde_send(vde_conn->conn, buf, len, flags);
+
+	int to_send_packets = 1;
+	to_send_packets += computeDuplicationCount(vde_conn->duplication_probability);	// Duplication handling
+
+	for (int i=0; i<to_send_packets; i++) {
+		vde_send(vde_conn->conn, buf, len, flags);
+	}
+
+	return 0;
 }
 
 static int vde_wirefilter_datafd(VDECONN *conn) {
@@ -108,4 +123,16 @@ static int vde_wirefilter_close(VDECONN *conn) {
 	int ret_value = vde_close(vde_conn->conn);
 	free(vde_conn);
 	return ret_value;
+}
+
+
+/* Returns the number of times a packet should be duplicated */
+int computeDuplicationCount(const double duplication_probability) {
+	int times = 0;
+
+	while (drand48() < (duplication_probability/100)) {
+		times++;
+	}
+
+	return times;
 }
